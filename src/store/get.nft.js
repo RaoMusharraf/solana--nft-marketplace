0f01f6a8c0b357a4b24d5c77e9893b56ede55549
Rao <RaoMusharraf>
2024-11-14 00:10:26 +0500

    .......

--- src/store/get.nft.js
+++ src/store/get.nft.js
@@ -3,7 +3,7 @@ export const getnft = async (mintHash) => {
         const response = await (await fetch(`http://localhost:5000/api/nft/getNftsByWallet/${mintHash}`)).json();
         return response.body;
     } catch (error) {
-        console.error('Error fetching digital asset by mintHash:', error);
+        console.error('Error fetching digital asset by mintHash :', error);
         throw error;
     }
 };
